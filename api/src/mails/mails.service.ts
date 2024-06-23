9b30d7fc4a35a6e84a226607f16b5c9cf6bf4320
Gustavo de Vito <gustavodevito38@gmail.com>
2024-06-22 22:53:25 -0300

    docs: add JSDoc

--- api/src/mails/mails.service.ts
+++ api/src/mails/mails.service.ts
@@ -11,11 +11,15 @@ export class MailsService {
     await this.client.connect();
   }
 
+  /**
+   * @description notification indicating that the specified product is in the alert zone.
+   * @param product The name of the product that is in the alert zone.
+   */
   alertLimit(product: string) {
     const content = `<div>O produto "<strong>${product}</strong>" está na zona de alerta.</div>`;
 
     this.client.emit(MailHelper.queue.topic, {
-      to: 'gustavodevito38@gmail.com',
+      to: 'example@example.com',
       subject: 'Alerta de Estoque Baixo - Notificação',
       content,
     });
